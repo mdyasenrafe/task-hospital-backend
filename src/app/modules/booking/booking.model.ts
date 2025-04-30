e77f32ad23c87d8040116c9e3da69430f8f15c5f
mdyasenrafe <mdyasenrafe@gmail.com>
2025-05-01 04:14:22 +0600

    booking model updated

--- src/app/modules/booking/booking.model.ts
+++ src/app/modules/booking/booking.model.ts
@@ -2,8 +2,8 @@ import { Schema, model, Document } from "mongoose";
 import { TBooking } from "./booking.type";
 
 const bookingSchema = new Schema<TBooking>({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  hospital: { type: Schema.Types.ObjectId, ref: "Hospital", required: true },
+  user: { type: Schema.Types.ObjectId, ref: "user", required: true },
+  hospital: { type: Schema.Types.ObjectId, ref: "hospital", required: true },
   service: { type: String, required: true },
   appointmentDate: { type: Date, required: true },
   status: {
